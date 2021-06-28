758bb7c62203b35cd179082b60253c729ec72695
Idrees <idreesmsyed1@gmail.com>
2021-06-27 20:32:15 -0400

    Fixed progress time display when no stream is playing

--- python_files/StreamPlayerVLC.py
+++ python_files/StreamPlayerVLC.py
@@ -51,21 +51,25 @@ class StreamPlayerVLC:
     def get_length(self):
         return self.format_length(self.player.get_length())
 
-    def get_current_time(self):
-        return self.format_length(self.player.get_time())
+    def get_current_time_formatted(self):
+        return self.format_length(self.get_current_time())
 
-    def get_time_milli(self):
-        return self.player.get_time()
+    def get_current_time(self):
+        """
+        Gets time in milliseconds. If no stream is playing return 0.
+        """
+        if self.player.get_time() == -1:
+            return 0
+        else:
+            return self.player.get_time()
 
     def seek_to_time(self, seek_time):
-        # self.player.set_time(self.player.get_time() + seek_time)
-        time = seek_time * self.player.get_length()
-        # print(int(time), " ", self.player.get_length())
-        self.player.set_time(int(time))
+        # Multiple the percentage of seek by total length to get seek time 
+        seek_time = seek_time * self.player.get_length()
+        self.player.set_time(int(seek_time))
 
     def get_percentage(self):
-        # print(round(self.player.get_time()/self.player.get_length(), 5))
-        return round(self.player.get_time() / self.player.get_length(), 5)
+        return round(self.get_current_time() / self.player.get_length(), 5)
 
     def format_length(self, length):
         millis = int(length)
