2a3b915e5eeea6d5387466b2c3b5298c869cf9ee
Idrees <idreesmsyed1@gmail.com>
2021-07-12 13:29:39 -0400

    Added channel attribute

--- python_files/SearchResult.py
+++ python_files/SearchResult.py
@@ -1,23 +1,30 @@
 class SearchResult:
 
     """
-    Contains the title and link of the result
+    Contains the Youtube ID, title and channel of the search result
     """
 
-    def __init__(self, title, yt_id):
-        self.title = title
-        # self.link = "https://www.youtube.com/watch?v=" + yt_id
+    def __init__(self, yt_id, title, channel):
         self.yt_id = yt_id
+        self.title = title
+        self.channel = channel
         self.thumbnail = "http://img.youtube.com/vi/{id}/0.jpg".format(id=yt_id)
+        self.link = "https://www.youtube.com/watch?v=" + yt_id
+
+    def get_yt_id(self):
+        return self.yt_id
 
     def get_title(self):
         return self.title
+
+    def get_channel(self):
+        return self.channel
 
     def get_thumbnail(self):
         return self.thumbnail
 
     def get_link(self):
-        return self.yt_id
+        return self.link
 
     def __str__(self):
         return self.get_title()
